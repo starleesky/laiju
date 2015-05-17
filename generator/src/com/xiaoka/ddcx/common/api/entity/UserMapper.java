9295627bfa9928c5aa48ab332a5d28171330323e
xin.li <xin.li@yangchediandian.com>
2015-05-17 13:12:28 +0800

    generator工程师自动生成mybatis的pojo映射文件

---
+++ generator/src/com/xiaoka/ddcx/common/api/entity/UserMapper.java
@@ -0,0 +1,17 @@
+package com.xiaoka.ddcx.common.api.entity;
+
+import com.xiaoka.ddcx.common.api.entity.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
